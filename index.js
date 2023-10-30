a04b370b87c8eadb78144c2479934b35fd32333f
Gen Vikat <gen@veebisepad.ee>
2023-10-30 13:23:49 +0200

    Change structure of the fail, add variables to the lessons.

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-// Objektid - andmestruktuur, mis koosneb võtmetest ja nendega seotud väärtustest. (key-value pairs)
+/**
+ * VARIABLES
+ * CONST - konstant, ehk väärtus, mida ei saa muuta hiljem
+ * LET - muutuja, mida saab hiljem koodis muuta
+ */
+
+const myFirstVariable = "This one we cant change later in code.";
+let mySecondVariable = "This one we can change later in code.";
+console.log(mySecondVariable);
+
+mySecondVariable = "Just like this";
+console.log(mySecondVariable);
 
 /**
+ * ANDMETÜÜBID
  * String => "Tere"
  * Number => 43
  * Boolean => true/false
  * Array => ["string", 32, true, {}, []],
- * Object => { key: "value", key2: "value2"} 
+ * Object => { key: "value", key2: "value2"}
  */
+
+// Objektid - andmestruktuur, mis koosneb võtmetest ja nendega seotud väärtustest. (key-value pairs)
 
 const whiteboard = {
     width: 1280,
     height: 900,
     brand: "iiyama",
     system: "iOS",
-    "screen color": "black"
-}
+    "screen color": "black",
+};
 
 /**
  * TYPE CONVERSIONS
  * Muuda andmetüüp teiseks andmetüübiks
- * 
+ *
  * String()
  * Number()
  * Boolean()
@@ -29,12 +43,12 @@ let matiiasID = "50624120019";
 console.log(typeof matiiasID);
 
 matiiasID = Number(matiiasID);
-console.log(typeof matiiasID)
+console.log(typeof matiiasID);
 
 /**
  * Comparisons
  * Võrdlus - võrdle andmeid ja andmetüüpe
- * 
+ *
  * == - Võrdleb väärtust ning tagastab Booleani
  * === - Võrdleb väärtust ja andmetüüpe ning tagastab Booleani
  */
@@ -44,4 +58,3 @@ const myRealAge = 55;
 
 console.log(myAge == myRealAge);
 console.log(myAge === myRealAge);
-
